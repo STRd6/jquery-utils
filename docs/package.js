f1ef95811667bdc072caff255a3c5a35f40aad7e
Daniel X Moore <yahivin@gmail.com>
2013-10-25 08:58:39 -0700

    :police_car::ng: Updated in browser at strd6.github.io/editor

--- docs/package.js
+++ docs/package.js
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html manifest="manifest.appcache?1382716717572">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
-<script src="http://strd6.github.io/tempest/javascripts/envweb.js"></script>
-<script src="http://strd6.github.io/require/v0.2.2.js"></script>
-</head>
-<body>
-<script>
-;(function(PACKAGE) {
-var require = Require.generateFor(PACKAGE);
-require('./main')
+(function(pkg) {
+  // Expose a require for our package so scripts can access our modules
+  window.require = Require.generateFor(pkg);
 })({
   "version": "0.1.1",
   "source": {
@@ -522,6 +512,3 @@ require('./main')
     "defaultBranch": "master"
   }
 });
-</script>
-</body>
-</html>
